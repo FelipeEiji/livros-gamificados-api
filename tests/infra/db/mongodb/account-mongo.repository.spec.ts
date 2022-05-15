8503731f473edb06734202273305a3d819d35067
Felipe Maruyama <felipe-eiji@hotmail.com>
2022-05-15 14:26:27 -0300

    test: add AccountMongoRepository

---
+++ tests/infra/db/mongodb/account-mongo.repository.spec.ts
@@ -0,0 +1,51 @@
+import { AccountMongoRepository, MongoHelper } from '@/infra/db';
+import { mockAddAccountParams } from '@/tests/domain/mocks';
+
+import { Collection } from 'mongodb';
+import faker from 'faker';
+
+const makeSut = (): AccountMongoRepository => {
+    return new AccountMongoRepository();
+};
+
+let accountCollection: Collection;
+
+describe('AccountMongoRepository', () => {
+    beforeAll(async () => {
+        await MongoHelper.connect(process.env.MONGO_URL);
+    });
+
+    afterAll(async () => {
+        await MongoHelper.disconnect();
+    });
+
+    beforeEach(async () => {
+        accountCollection = MongoHelper.getCollection('accounts');
+        await accountCollection.deleteMany({});
+    });
+
+    describe('add()', () => {
+        test('Should return an account on success', async () => {
+            const sut = makeSut();
+            const addAccountParams = mockAddAccountParams();
+            const isValid = await sut.add(addAccountParams);
+            expect(isValid).toBe(true);
+        });
+    });
+
+    describe('checkByEmail()', () => {
+        test('Should return true if email is valid', async () => {
+            const sut = makeSut();
+            const addAccountParams = mockAddAccountParams();
+            await accountCollection.insertOne(addAccountParams);
+            const exists = await sut.checkByEmail(addAccountParams.email);
+            expect(exists).toBe(true);
+        });
+
+        test('Should return false if email is not valid', async () => {
+            const sut = makeSut();
+            const exists = await sut.checkByEmail(faker.internet.email());
+            expect(exists).toBe(false);
+        });
+    });
+});
