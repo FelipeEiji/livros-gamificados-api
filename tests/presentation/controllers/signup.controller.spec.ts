f93a91c1e16bcf13a440c684de8995275fad2557
Felipe Maruyama <felipe-eiji@hotmail.com>
2022-05-15 11:18:19 -0300

    test: ensure SignUpController uses Validation properly

---
+++ tests/presentation/controllers/signup.controller.spec.ts
@@ -0,0 +1,46 @@
+import { SignUpController } from '@/presentation/controllers';
+import { MissingParamError } from '@/presentation/errors';
+import { ValidationSpy } from '@/tests/presentation/mocks';
+import { badRequest } from '@/presentation/helpers';
+
+import * as faker from 'faker';
+
+const mockRequest = (): SignUpController.Request => {
+    const password = faker.internet.password();
+    return {
+        name: faker.name.findName(),
+        email: faker.internet.email(),
+        password,
+        passwordConfirmation: password,
+    };
+};
+
+type SutTypes = {
+    sut: SignUpController;
+    validationSpy: ValidationSpy;
+};
+
+const makeSut = (): SutTypes => {
+    const validationSpy = new ValidationSpy();
+    const sut = new SignUpController(validationSpy);
+    return {
+        sut,
+        validationSpy,
+    };
+};
+
+describe('SignUp Controller', () => {
+    test('Should call Validation with correct value', async () => {
+        const { sut, validationSpy } = makeSut();
+        const request = mockRequest();
+        await sut.handle(request);
+        expect(validationSpy.input).toEqual(request);
+    });
+
+    test('Should return 400 if Validation returns an error', async () => {
+        const { sut, validationSpy } = makeSut();
+        validationSpy.error = new MissingParamError(faker.random.word());
+        const httpResponse = await sut.handle(mockRequest());
+        expect(httpResponse).toEqual(badRequest(validationSpy.error));
+    });
+});
