25f644d20bb71a8c2647055d8f472db10538a6fc
Felipe Maruyama <felipe-eiji@hotmail.com>
2022-05-15 11:18:13 -0300

    feat: ensure SignUpController uses Validation properly

--- src/presentation/controllers/signup.controller.ts
+++ src/presentation/controllers/signup.controller.ts
@@ -1,1 +1,27 @@
-export class SignUpController {}
+import { Controller, HttpResponse, Validation } from '@/presentation/protocols';
+import { badRequest, serverError, ok } from '@/presentation/helpers';
+
+export class SignUpController implements Controller {
+    constructor(private readonly validation: Validation) {}
+
+    async handle(request: SignUpController.Request): Promise<HttpResponse> {
+        try {
+            const error = this.validation.validate(request);
+            if (error) {
+                return badRequest(error);
+            }
+            return ok({});
+        } catch (error) {
+            return serverError(error);
+        }
+    }
+}
+
+export namespace SignUpController {
+    export type Request = {
+        name: string;
+        email: string;
+        password: string;
+        passwordConfirmation: string;
+    };
+}
