d16e96391877548a2c91915373be6d3ea0fdfe43
Felipe Maruyama <felipe-eiji@hotmail.com>
2022-05-24 21:59:24 -0300

    refactor: remove unused imports from signup.factory.ts

--- src/main/factories/controllers/signup.factory.ts
+++ src/main/factories/controllers/signup.factory.ts
@@ -1,6 +1,4 @@
 import { SignUpController } from '@/presentation/controllers';
-import { EmailValidation } from '@/validation/validators';
-import { EmailValidatorAdapter } from '@/infra/validators/email-validator.adapter';
 import { makeDbAddAccount } from '@/main/factories/usecases/add-account.factory';
 import { makeSignUpValidation } from '@/main/factories/controllers/signup-validation.factory';
 
