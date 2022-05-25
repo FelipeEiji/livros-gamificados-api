158c31b0edbd488943c43275a9de61b47d85e2dd
Felipe Maruyama <felipe-eiji@hotmail.com>
2022-05-24 21:59:01 -0300

    feat: implements new validations

--- src/main/factories/controllers/signup.factory.ts
+++ src/main/factories/controllers/signup.factory.ts
@@ -2,16 +2,14 @@ import { SignUpController } from '@/presentation/controllers';
 import { EmailValidation } from '@/validation/validators';
 import { EmailValidatorAdapter } from '@/infra/validators/email-validator.adapter';
 import { makeDbAddAccount } from '@/main/factories/usecases/add-account.factory';
+import { makeSignUpValidation } from '@/main/factories/controllers/signup-validation.factory';
 
 export const makeSignUpController = (): SignUpController => {
-    const emailValidation = new EmailValidation(
-        'email',
-        new EmailValidatorAdapter(),
-    );
+    const signUpValidation = makeSignUpValidation();
     const dbAddAccount = makeDbAddAccount();
     const signUpController = new SignUpController(
         dbAddAccount,
-        emailValidation,
+        signUpValidation,
     );
     return signUpController;
 };
