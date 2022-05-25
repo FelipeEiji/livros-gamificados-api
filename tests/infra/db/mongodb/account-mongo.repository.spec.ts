98875d9622ce8eb84d6e9be63715f68eea9339c1
Felipe Maruyama <felipe-eiji@hotmail.com>
2022-05-24 22:56:43 -0300

    test: create loadByEmail and updateAccessToken functions for AccountMongoRepository

--- tests/infra/db/mongodb/account-mongo.repository.spec.ts
+++ tests/infra/db/mongodb/account-mongo.repository.spec.ts
@@ -48,4 +48,46 @@ describe('AccountMongoRepository', () => {
             expect(exists).toBe(false);
         });
     });
+
+    describe('loadByEmail()', () => {
+        test('Should return an account on success', async () => {
+            const sut = makeSut();
+            const addAccountParams = mockAddAccountParams();
+            await accountCollection.insertOne(addAccountParams);
+            const account = await sut.loadByEmail(addAccountParams.email);
+            expect(account).toBeTruthy();
+            expect(account.id).toBeTruthy();
+            expect(account.name).toBe(addAccountParams.name);
+            expect(account.password).toBe(addAccountParams.password);
+        });
+
+        test('Should return null if loadByEmail fails', async () => {
+            const sut = makeSut();
+            const account = await sut.loadByEmail(faker.internet.email());
+            expect(account).toBeFalsy();
+        });
+    });
+
+    describe('updateAccessToken()', () => {
+        test('Should update the account accessToken on success', async () => {
+            const sut = makeSut();
+            const res = await accountCollection.insertOne(
+                mockAddAccountParams(),
+            );
+            const fakeAccount = await accountCollection.findOne({
+                _id: res.insertedId,
+            });
+            expect(fakeAccount.accessToken).toBeFalsy();
+            const accessToken = faker.datatype.uuid();
+            await sut.updateAccessToken(
+                fakeAccount._id.toHexString(),
+                accessToken,
+            );
+            const account = await accountCollection.findOne({
+                _id: fakeAccount._id,
+            });
+            expect(account).toBeTruthy();
+            expect(account.accessToken).toBe(accessToken);
+        });
+    });
 });
